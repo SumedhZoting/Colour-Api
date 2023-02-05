8c92c6f9207936e3e4ac0e340c7426f057ae3300
SumedhZoting <123283676+SumedhZoting@users.noreply.github.com>
2023-02-05 20:56:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,35 +10,54 @@ app.use(bodyParser.json());
 app.use(cors())
 
 app.post("/hex", (req, res) => {
-    ColorThief.getColor(req.body.imgurl)
-        .then(color => {
-            domcolour = rgbToHex(color[0], color[1], color[2])
-            res.send(domcolour)
-        })
+  let colour = []
+  ColorThief.getColor(req.body.imgurl)
+    .then(color => {
+      domcolour = rgbToHex(color[0], color[1], color[2])
+      colour.push(domcolour)
+      res.send(domcolour)
+    })
 })
 
 app.post("/rgb", (req, res) => {
-    ColorThief.getColor(req.body.imgurl)
-        .then(color => {
-            res.send(color)
-        })
+  ColorThief.getColor(req.body.imgurl)
+    .then(color => {
+      res.send(color)
+    })
 });
 
 app.post("/palette", (req, res) => {
-    ColorThief.getPalette(req.body.imgurl, 5)
-        .then(palette => { res.send(palette) })
-        .catch(err => { console.log(err) })
+  if (req.body.imgurl == null) { return; }
+  ColorThief.getPalette(req.body.imgurl, 5)
+    .then(palette => {
+      hexcolours = []
+      i = 0;
+      while (i != 5) {
+        colour = rgbToHex(palette[i][0], palette[i][1], palette[i][2])
+        hexcolours.push(colour)
+        i = i + 1
+      }
+      res.send(hexcolours)
+    })
 })
 
 function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 app.listen(8080, () => {
-    console.log("server started")
+  console.log("server started")
 })
+
+function getdom(url) {
+  ColorThief.getColor(url)
+    .then(color => {
+      domcolour = rgbToHex(color[0], color[1], color[2])
+      return domcolour.toString()
+    })
+}
